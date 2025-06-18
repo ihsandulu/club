7fed7689b5aad1c4e32e24f3561f00429434a24f
ihsandulu <ihsan.dulu@gmail.com>
2025-06-18 07:41:31 +0700

    test domain

--- app/Models/Login_m.php
+++ app/Models/Login_m.php
@@ -57,7 +57,7 @@ class Login_m extends Core_m
                 }
             }
         }
-        // dd($data);
+        dd($data);
 
         // echo "ngaco";die;
         if (isset($_POST["user_phone"]) && isset($_POST["user_token"])) {
